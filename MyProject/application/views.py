b4ea2f0f2954e859d200a6565340ab59bee0350b
Dhyanesh Supare <dhyaneshsupare712@gmail.com>
2024-06-15 19:09:27 +0530

    First commit

---
+++ MyProject/application/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, HttpResponse
+from application.models import *
+from django.contrib import messages
+
+# Create your views here.
+
+def index(request):
+    if request.method == "POST":
+        a = request.POST.get('name')
+        b = request.POST.get('email')
+        c = request.POST.get('contact')
+        d = request.POST.get('message')
+        enquiry = Form(name = a, email = b, contact = c, message = d)
+        enquiry.save()
+
+        messages.success(request, "Thanks for Interacting with us......!")
+    return render(request, 'index.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def blogdetails(request):
+    return render(request, 'blog-details.html')
